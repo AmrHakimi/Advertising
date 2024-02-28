984ab6119868b929862129e21d6a5e5091b86a0c
AmrHakimi <161561072+AmrHakimi@users.noreply.github.com>
2024-02-28 16:17:53 +0800

    Update Advertising.py

--- Advertising.py
+++ Advertising.py
@@ -11,9 +11,9 @@ This app predicts the **Advertising Sales** type!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV = st.sidebar.slider('TV', 0.7, 297, 100)
-    Radio = st.sidebar.slider('Radio', 0, 50, 15)
-    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114, 20)
+    TV = st.sidebar.slider('TV', 0.7, 297.0, 100.0)
+    Radio = st.sidebar.slider('Radio', 0.0, 50.0, 15.0)
+    Newspaper = st.sidebar.slider('Newspaper', 0.3, 114.0, 20.0)
     data = {'TV': TV,
             'Radio': Radio,
             'Newspaper': Newspaper}
