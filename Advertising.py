fa20bb120a2de7ac985688498bca6f22311dd23a
AmrHakimi <161561072+AmrHakimi@users.noreply.github.com>
2024-02-28 16:01:07 +0800

    Update Advertising.py

--- Advertising.py
+++ Advertising.py
@@ -19,15 +19,3 @@ def user_input_features():
             'Newspaper': Newspaper}
     features = pd.DataFrame(data, index=[0])
     return features
-
-df = user_input_features()
-
-st.subheader('User Input parameters')
-st.write(df)
-
-loaded_model = pickle.load(open("Advertisingmodel.h5", "rb"))
-
-prediction = loaded_model.predict(df)
-
-st.subheader('Prediction')
-st.write(prediction)
